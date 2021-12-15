e49981fd53782c60fc164e25cef25a8814c25781
Sven <61794662+SvenKeimpema@users.noreply.github.com>
2021-12-15 10:40:44 +0100

    Add files via upload

---
+++ Opdracht 2/main.js
@@ -0,0 +1,48 @@
+function translateToDutch() {
+
+}
+
+function translateToEnglish() {
+
+}
+
+
+function checkSelected(currVar) {
+
+}
+
+function checkAanhef() {
+
+}
+
+function checkNaam() {
+    
+}
+
+function checkAdres() {
+    
+}
+
+function checkWoonplaats() {
+
+}
+
+
+function checkPostcode() {
+ 
+}
+
+function checkGeboortedatum() {
+ 
+}
+
+function checkNationaliteit() {
+    
+}
+
+function checkBeroep() {
+
+}
+
+function berekenLeeftijd(geboortedatum) {
+}
